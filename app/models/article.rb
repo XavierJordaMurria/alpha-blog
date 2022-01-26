afa4af3ed9578a898c3fd9cc8336afdb44d0b5ae
Xavi Jorda <x.jorda@cognigy.com>
2022-01-26 07:36:25 +0100

    adding show articles page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
-class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 6, maximum: 100}
-    validates :description, presence: true, length: { minimum: 10, maximum: 100}
-end
+class Article < ActiveRecord::Base
+    # We discovered in our rails console that if you enter an empty article it will still register to the
+    #database which is very bad. So the point of this model is to add validations to make sure that doesnt happen!
+  
+    #We also want to add length validation because if a title and or desacription is one character, that doesn't
+    # make much sense.
+    validates :title, presence: true, length: { minimum: 3, maximum: 50}
+    validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  
+  end
